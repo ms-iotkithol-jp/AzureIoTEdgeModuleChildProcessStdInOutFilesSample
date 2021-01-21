12e53cdec06625bd1b2ba25ae632ceb616be5fa9
ms-iotkithol-jp <ms-iotkithol-jp@outlook.jp>
2021-01-21 16:51:49 +0900

    First Draft

---
+++ source/ConsoleAppChild/Program.cs
@@ -0,0 +1,48 @@
+﻿//#define LINUX
+using System;
+using System.IO;
+
+namespace ConsoleAppChild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Child!");
+            try
+            {
+                string filename = "";
+#if LINUX
+                filename = "/data/test.txt";
+#else
+                filename = "c:\\data\\test.txt";
+#endif
+                var currentDir = Directory.GetCurrentDirectory();
+                Console.WriteLine($"{currentDir}");
+                using (var file = File.OpenWrite(filename))
+                {
+                    using (var writer = new StreamWriter(file))
+                    {
+                        writer.WriteLine("test!");
+                        writer.Flush();
+                        Console.WriteLine("Wrote file!");
+                    }
+                }
+                while (true)
+                {
+                    Console.WriteLine("Waiting input from parent");
+                    string line = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        Console.WriteLine($"Child Received - '{line}'");
+                        if (line.Trim().ToLower() == "quit") break;
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Exception - {ex.Message}");
+            }
+        }
+    }
+}
---
+++ source/ConsoleAppInOutTest/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleAppInOutTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Parent");
+            Console.WriteLine($"CurrentDir={Directory.GetCurrentDirectory()}");
+
+            var spInfo = new ProcessStartInfo();
+            spInfo.FileName = args[0];
+            spInfo.UseShellExecute = false;
+            spInfo.RedirectStandardOutput = true;
+            spInfo.RedirectStandardError = true;
+            spInfo.RedirectStandardInput = true;
+
+            var process = new Process();
+            process.StartInfo = spInfo;
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            while (true)
+            {
+                lock (process)
+                {
+                    if (outputCount > 0) break;
+                }
+                Console.WriteLine("Waiting for Child process ready");
+                Task.Delay(1000).Wait();
+            }
+
+            int index = 0;
+            while (index < 10)
+            {
+                string line = $"test{index++}";
+                Console.WriteLine(line);
+                process.StandardInput.WriteLine(line);
+                Console.WriteLine("transfered");
+                Task.Delay(1000).Wait();
+            }
+            process.StandardInput.WriteLine("quit");
+            Console.WriteLine("Waiting for Child end");
+            process.WaitForExit();
+
+        }
+
+        private static int outputCount = 0;
+        private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine($"Child Error: {e.Data}");
+        }
+
+        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine($"Child Output: {e.Data}");
+            outputCount++;
+        }
+    }
+}
